5e73107c6729610c608cdb8c7449aca94d4a984a
Suraj Kumar <surajk2072003@gmail.com>
2025-09-10 19:15:03 +0530

    Add files via upload

---
+++ CalculatorExample.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class CalculatorExample {
+    public static void main(String[] args) {
+    Scanner obj = new Scanner(System.in);
+
+    System.out.println("Enter the operator (+, -, *, /, %): ");
+    char operator = obj.next() .charAt(0);
+
+    System.out.println("Enter the first number: ");
+    int a = obj.nextInt();
+
+    System.out.println("Enter Second Number: ");
+    int b = obj.nextInt();
+
+    
+    double result;
+    switch (operator) {
+        case '+':
+        result = a + b;
+        System.out.println(result);
+        break;
+
+        case '-':
+        result = a - b;
+        System.out.println(result);
+        break;
+
+        case '*':
+        result = a * b;
+        System.out.println(result);
+        break;
+
+        case '/':
+        result = a / b;
+        System.out.println(result);
+        break;
+
+        case '%':
+        result = a % b;
+        System.out.println(result);
+        break;
+
+        default:
+        System.out.println("Invalid operator");
+        break;
+        }
+        }
+
+}
