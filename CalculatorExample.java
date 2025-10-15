23fba89aaa2ccbf78be7cb80646d208a09ff5f58
Suraj Kumar <199711515+surajkumar071@users.noreply.github.com>
2025-10-15 06:41:13 +0530

    Update CalculatorExample.java

--- CalculatorExample.java
+++ CalculatorExample.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class CalculatorExample           B{
+class CalculatorExample         {
     public static void main(String[] args) {
     Scanner obj = new Scanner(System.in);
 
